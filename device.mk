507ad8403f1e14b9e9a9df951044f3fc4c7283d8
Sean Du <do4suki@gmail.com>
2025-02-21 09:52:02 +0800

    oneplus12: Rework device kernel building
    Change-Id: Iaaad596eb5655b6b07c393489d10dc9d90b6bba5
    Signed-off-by: Sean Du <do4suki@gmail.com>

--- device.mk
+++ device.mk
@@ -29,6 +29,9 @@ PRODUCT_ODM_PROPERTIES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/display_id_4630946450791512195.xml:$(TARGET_COPY_OUT_VENDOR)/etc/displayconfig/display_id_4630946450791512195.xml
 
+# Kernel
+KERNEL_PREBUILT_DIR := $(LOCAL_PATH)-kernel
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay-lineage
